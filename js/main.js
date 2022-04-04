1b52a80a3e47b7328be02b50a70391b9c18d94eb
tesh510 <hiteshdeo@yahoo.com>
2022-04-04 15:32:50 -0700

    jselements

--- js/main.js
+++ js/main.js
@@ -4,18 +4,23 @@
 
 
 /*----- app's state (variables) -----*/
-
+let winner;
+let gameBoard;
+let turn;
 
 
 
 /*----- cached element references -----*/
-
+const squares = document.querySelectorAll('td div');
+const message = document.querySelector('h1');
+const resetBtn = document.getElementById('reset-btn');
 
 
 
 /*----- event listeners -----*/
 
-
+document.querySelector('table').addEventListener('click', handleMove);
+document.querySelector('button').addEventListener('click', initialize);
 
 
 /*----- functions -----*/
